acc3f54cec6bdbf8faeb070307e6d17081d3f462
LC271 <lukasz.ciochon@microsoft.wsei.edu.pl>
2026-03-04 22:53:22 +0100

    Refactor constructor of BaseGradeBook - dodawanie bool 'a waga.

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -6,7 +6,7 @@ namespace GradeBook.GradeBooks
 {
     public class RankedGradeBook : BaseGradeBook
     {
-        public RankedGradeBook(string name) : base(name)
+        public RankedGradeBook(string name, bool waga) : base(name, waga)
         {
             Type = GradeBookType.Ranked;
         }
